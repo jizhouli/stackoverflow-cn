a3383b078d61920c19698554d8b7d68f64665d3c
Justin <justinli.ljt@gmail.com>
2015-04-08 11:46:34 +0800

    add dump_parse.py file

---
+++ backend/store/dump_parse.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Justin<justinli.ljt@gmail.com>
+# 2015-04-08
+'''
+'''
+
+import os
+import sys
+
+class DumpParse(object):
+    '''
+    parse dump data from stackoverflow
+    '''
+    def __init__(self):
+        pass
+    def __str__(self):
+        pass
+    pass
+
+if __name__ == '__main__':
+    dp = DumpParse()
+
